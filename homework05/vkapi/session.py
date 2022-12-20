8f0ddde7116df2a34397bc4a36b752ccdd24bbc8
firs-iln <firs_iln@mail.ru>
2022-12-20 21:12:24 +0300

    Update session.py

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -1,13 +1,13 @@
 import typing as tp
 
 import requests
-from requests.adapters import HTTPAdapter, Retry
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 class Session:
     """
     Сессия.
-
     :param base_url: Базовый адрес, на который будут выполняться запросы.
     :param timeout: Максимальное время ожидания ответа от сервера.
     :param max_retries: Максимальное число повторных запросов.
@@ -21,45 +21,22 @@ class Session:
         max_retries: int = 3,
         backoff_factor: float = 0.3,
     ) -> None:
-        self.base_url = base_url
-        self.timeout = timeout
-        self.max_retries = max_retries
-        self.backoff_factor = backoff_factor
-        self.session = None
-
-    def get(self, url: str, params=None, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        session = self.retry_session()
-        response = session.get(
-            self.base_url + f"/{url}", *args, **kwargs, timeout=self.timeout, params=params
+        self._session = requests.Session()
+        self._session.mount(
+            "https://",
+            HTTPAdapter(
+                max_retries=Retry(
+                    total=max_retries,
+                    backoff_factor=backoff_factor,
+                    status_forcelist=[429, 500, 502, 503, 504],
+                )
+            ),
         )
-        if response.status_code == 200:
-            return response
-        raise requests.exceptions.RetryError
+        self._base_url = base_url
+        self._timeout = timeout
 
-    def post(
-        self, url: str, params=None, data=None, *args: tp.Any, **kwargs: tp.Any
-    ) -> requests.Response:
-        session = self.retry_session()
-        response = session.post(
-            self.base_url + f"/{url}",
-            *args,
-            **kwargs,
-            timeout=self.timeout,
-            params=params,
-            data=data,
-        )
-        if response.status_code == 200:
-            return response
-        raise requests.exceptions.RetryError
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        return self._session.get(self._base_url + "/" + url, params=kwargs, timeout=self._timeout)
 
-    def retry_session(self):
-        if not self.session:
-            self.session = requests.Session()
-            retries = Retry(
-                total=self.max_retries,
-                backoff_factor=self.backoff_factor,
-                raise_on_status=False,
-                status_forcelist=[500, 502, 503, 504],
-            )
-            self.session.mount("https://", HTTPAdapter(max_retries=retries))
-        return self.session
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        return self._session.post(self._base_url + "/" + url, data=kwargs)
